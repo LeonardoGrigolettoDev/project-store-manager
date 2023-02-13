4323d02551e48032f1f7db07b7e5068dd714a405
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2023-02-13 18:41:57 -0300

    realiza o primeiro requisito

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
-const express = require('express');
+const express = require("express");
+const connection = require("./db/connection");
+const productsModel = require('./models/products.model')
 
 const app = express();
 
+const HTTP_OK_STATUS = 200;
+
+app.use(express.json());
+
+app.get("/products", async (req, res) => {
+  productsModel.findAllProducts(req, res)
+});
+
+app.get("/products/:id", async (req, res) => {
+  productsModel.findProductById(req, res)
+});
+
 // não remova esse endpoint, é para o avaliador funcionar
-app.get('/', (_request, response) => {
+app.get("/", (_request, response) => {
   response.send();
 });
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
